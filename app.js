aefe603779079dc184ca327c60e7566542eca586
Jaine Santos <jaine@valarisolutions.com>
2022-10-31 17:49:53 +0100

    chore: remove console logs

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
+const { argv } = require('process');
 const { createFile } = require('./helpers/multiplica');
-const argv = require('yargs').argv;
-
 console.clear();
-console.log(process.argv);
-console.log(argv);
-
-console.log('base: yargs', argv.base);
-
-
 
 /**
  * To create all tables once
  */
+
 // const bases = [
 //     1,2,3,4,5,6,7,8,9,10
 // ]
 // bases.forEach(base => {
 //     createFile(base).then(fileName => console.log(`${fileName} created`))
 //     .catch(err => {throw err})
-    
+
 // });
 /**
  * To use a terminal command
@@ -26,5 +20,8 @@ console.log('base: yargs', argv.base);
 
 // const [, , arg3 = 'base=0'] = process.argv;
 // const [ , base = 0] = arg3.split('=') 
-// createFile(base).then(fileName => console.log(`${fileName} created`))
-// .catch(err => {throw err})
+
+
+
+createFile(argv.b, argv.l).then(fileName => console.log(`${fileName} created`))
+    .catch(err => { throw err })
