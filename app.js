cdd551521ab624fc148e954e8a3f1a0779b95b04
Jaine Santos <jaine@valarisolutions.com>
2022-10-31 14:18:06 +0100

    fix: handle the input arg as console command

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const { createFile } = require('./helpers/multiplica');
 
 console.clear();
-
+/**
+ * To create all tables once
+ */
 // const bases = [
 //     1,2,3,4,5,6,7,8,9,10
 // ]
@@ -11,5 +13,7 @@ console.clear();
     
 // });
 
-createFile(base=2).then(fileName => console.log(`${fileName} created`))
+const [, , arg3 = 'base=0'] = process.argv;
+const [ , base = 0] = arg3.split('=') 
+createFile(base).then(fileName => console.log(`${fileName} created`))
 .catch(err => {throw err})
